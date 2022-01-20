db1bb82c63dcd4bc9608c9d24b450b6a5eb672e8
galangdavianpradana <galangdavianp>
2022-01-20 14:11:45 +0700

    pertemuan-1

---
+++ Pertemuan 1/catatan.js
@@ -0,0 +1,87 @@
+//identifier
+
+/**
+ * identifier adalah penamaan variable, fungsi ataupun parameter
+ * karakter pertamanya hanya bisa huruf a-z atau A-Z dan simbol underscore atau dollar
+ * tidak boleh menggunakan angka di depan
+ * tidak boleh menggunakan bahasa yang sudah ada default di js
+ */
+
+
+
+
+
+
+
+
+
+
+//statement
+
+/**
+ * statement = suatu pernyataan js yang paling dasar dan memiliki tugas tertentu
+ */
+
+// let name = "Hello Beb" // Statement dasar/biasa
+// console.log(name)
+
+
+
+
+
+
+
+
+
+
+//expression
+
+/**
+ * expression merupakan expressi js yang berfungsi untuk menghasilkan nilai tertentu
+ */
+
+
+
+
+
+
+
+
+
+
+//scope
+
+/**
+ * Scope adalah cakupan kode agar bisa mengakses atau membatasi
+ */
+
+
+
+//global scope
+/**
+ * global scope merupakan cakupan di seluruh file
+ */
+
+
+//local scope
+/**
+ * local scope 
+ */
+
+
+
+//block scope
+/**
+ * block scope hanya bisa di panggil di circle tertentu aja
+ */
+
+
+
+
+
+
+/**
+ * const bisa di ubah
+ * let bisa di ubah 
+ * var bisa di ubah // kecuali di dalam block scope karena dia global //
+ */
---
+++ Pertemuan 1/test.js
@@ -0,0 +1,92 @@
+//identifier
+
+
+
+
+
+
+
+
+
+
+
+//statement
+
+// let name = "Hello Beb" // Statement dasar/biasa
+// console.log(name)
+
+
+
+
+
+
+
+
+
+
+//expression
+
+// let first = 10
+// let second = 1
+// let result = first + second
+
+// if(result>1){
+//     console.log('Kita  tanggal ' + result)
+// }
+
+
+
+
+
+
+
+
+
+
+//scope
+
+
+
+//global scope
+
+// var a = 'Hallo pacar saya'
+// function first(){
+//     console.log(a)
+// }
+
+// function second(){
+//     a = 'Kita dah jadi pacar kan?'
+//     console.log(a)
+// }
+// first()
+// second()
+
+// const a = 'mobil'
+// function kendaraan(){
+//     console.log(a)
+//     if(true){
+//         const b = 'motor'
+//         console.log(b)
+//     }
+// }
+// kendaraan()
+
+
+
+
+
+
+
+
+
+
+//!TUGAS
+
+var a = 'Alan'
+function ganteng (){
+    console.log(a)
+    if(true){
+        const b = 'Bedamage'
+        console.console.log(b)
+    }
+}
